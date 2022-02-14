99b1fd96d481e9830f588dffc181a53573487b19
jiminnn95 <jimin8338@gmail.com>
2022-02-14 16:04:19 +0900

    Change Naming for readability

--- EMS/EMS/Editor.h
+++ EMS/EMS/Editor.h
@@ -7,5 +7,5 @@ class Editor {
 	bool ADD(Employee data);
 	bool DEL(Column type, string data);
 	bool SCH(Column type, string data);
-	bool MOD(Column type1, string data1, Column type2, string data2);
+	bool MOD(Column typeFrom, string dataFrom, Column typeTo, string dataTo);
 };
