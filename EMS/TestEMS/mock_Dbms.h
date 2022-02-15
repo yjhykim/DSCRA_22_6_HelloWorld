0f0215a9ef162dd40c965850ed29154bb3588d86
Yongjun Kim <yjhykim@gmail.com>
2022-02-15 16:56:06 +0900

    [interface 수정] Dbms의 interface 수정
    Add의 return이 void로 변경되어 적용합니다.

--- EMS/TestEMS/mock_Dbms.h
+++ EMS/TestEMS/mock_Dbms.h
@@ -6,7 +6,7 @@
 
 class mock_Dbms : public IDbms {
 public:
-	MOCK_METHOD(list<Employee*>, add, (Employee data), (override));
+	MOCK_METHOD(void, add, (Employee data), (override));
 	MOCK_METHOD(list<Employee*>, del, (Column type, string data), (override));
 	MOCK_METHOD(list<Employee*>, sch, (Column type, string data), (override));
 	MOCK_METHOD(list<Employee*>, mod, (Column typeFrom, string dataFrom, Column typeTo, string dataTo), (override));
