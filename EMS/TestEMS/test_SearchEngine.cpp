fc90091735446c292ff39920123d80c147bc6070
jiminnn95 <jimin8338@gmail.com>
2022-02-15 17:17:20 +0900

    [Test/Fix] Add test for SearchEngine, fix argument
    - SearchEngine의 함수들을 위한 UnitTest를 구현하였습니다.
    - enum의 경우 reference 타입이 안되어 인자 타입을 수정하였습니다.

--- EMS/TestEMS/test_SearchEngine.cpp
+++ EMS/TestEMS/test_SearchEngine.cpp
@@ -9,7 +9,7 @@ class FixtureSearchEngine : public ::testing::Test {
 protected:
     void SetUp() override {
         db.emplace_back(Employee{ 2015123099, {"VXIHXOTH", "JHOP"}, CL::CL3, {010, 3112, 2609}, {1977,12,11}, CERTI::ADV });
-        db.emplace_back(Employee{ 2017112609, {"FB", "NTAWR"}, CL::CL4, {010, 5645, 6122}, {1986,12,03}, CERTI::PRO });
+        db.emplace_back(Employee{ 2017112609, {"FB", "NTAWR"}, CL::CL4, {010, 5645, 6122}, {1986,12,3}, CERTI::PRO });
         db.emplace_back(Employee{ 2018115040, {"TTETHU", "HBO"}, CL::CL3, {010, 4581, 2050}, {2008,07,18}, CERTI::ADV });
         db.emplace_back(Employee{ 1988114052, {"NQ", "LVARW"}, CL::CL4, {010, 4528, 3059}, {1991,10,21}, CERTI::PRO });
         db.emplace_back(Employee{ 2019129568, {"SRERLALH", "HMEF"}, CL::CL2, {010, 3091, 9521}, {1964,9,10}, CERTI::PRO });
@@ -52,16 +52,59 @@ TEST_F(FixtureSearchEngine, SearchByEmployeeNum) {
 TEST_F(FixtureSearchEngine, SearchByName) {
     SearchEngine se;
 
+    auto result = se.search(db, Name{ "VXIHXOTH", "JHOP" });
+    ASSERT_EQ(1, result.size());
+    auto item = result.begin();
+    EXPECT_EQ(2015123099, (*item)->employeeNum);
+
+    result = se.search(db, Name{ "VCUHLE", "" });
+    ASSERT_EQ(1, result.size());
+    item = result.begin();
+    EXPECT_EQ(2005101762, (*item)->employeeNum);
+
+    result = se.search(db, Name{ "", "VNUP" });
+    ASSERT_EQ(1, result.size());
+    item = result.begin();
+    EXPECT_EQ(2008108827, (*item)->employeeNum);
 }
 
 TEST_F(FixtureSearchEngine, SearchByCL) {
     SearchEngine se;
 
+    auto result = se.search(db, CL::CL2);
+
+    ASSERT_EQ(2, result.size());
+    auto item = result.begin();
+    EXPECT_EQ(2019129568, (*item)->employeeNum); item = next(item);
+    EXPECT_EQ(2003113260, (*item)->employeeNum); item = next(item);
+
+    result = se.search(db, CL::CL1);
+    ASSERT_EQ(5, result.size());
+
+    result = se.search(db, CL::CL3);
+    ASSERT_EQ(4, result.size());
+
+    result = se.search(db, CL::CL4);
+    ASSERT_EQ(9, result.size());
 }
 
 TEST_F(FixtureSearchEngine, SearchByPhoneNum) {
     SearchEngine se;
 
+    auto result = se.search(db, PhoneNum{ 010, 9031, 2726 });
+    ASSERT_EQ(1, result.size());
+    auto item = result.begin();
+    EXPECT_EQ(2008108827, (*item)->employeeNum);
+
+    result = se.search(db, PhoneNum{ -1, 7174 , -1 });
+    ASSERT_EQ(1, result.size());
+    item = result.begin();
+    EXPECT_EQ(2001122329, (*item)->employeeNum);
+
+    result = se.search(db, PhoneNum{ -1, -1, 8566 });
+    ASSERT_EQ(1, result.size());
+    item = result.begin();
+    EXPECT_EQ(2011109136, (*item)->employeeNum);
 }
 
 
@@ -71,11 +114,24 @@ TEST_F(FixtureSearchEngine, SearchByBirthDay) {
     auto result = se.search(db, Birthday{ 1978, -1, -1 });
     ASSERT_EQ(2, result.size());
     auto item = result.begin();
-    EXPECT_EQ("RTAH", (*item)->name.first);
+    EXPECT_EQ(2008108827, (*item)->employeeNum); item = next(item);
+    EXPECT_EQ(1985125741, (*item)->employeeNum); item = next(item);
+
+    result = se.search(db, Birthday{ -1, 2, -1 });
+    EXPECT_EQ(3, result.size());
+
+    result = se.search(db, Birthday{ -1, -1, 18 });
+    EXPECT_EQ(3, result.size());
 }
 
 
 TEST_F(FixtureSearchEngine, SearchByCerti) {
     SearchEngine se;
 
+    auto result = se.search(db, CERTI::ADV);
+    EXPECT_EQ(8, result.size());
+    result = se.search(db, CERTI::PRO);
+    EXPECT_EQ(12, result.size());
+    result = se.search(db, CERTI::EX);
+    EXPECT_EQ(0, result.size());
 }
