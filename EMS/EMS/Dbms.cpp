1a61fe025256012748feff0ddb9dffaa964c8b71
Yongjun Kim <yjhykim@gmail.com>
2022-02-16 10:50:58 +0900

    [dbms] 모듈 세부 내용 구현
    dbms 내부 각 query에 대한 세부 내용을 구현 진행

--- EMS/EMS/Dbms.cpp
+++ EMS/EMS/Dbms.cpp
@@ -1,25 +1,84 @@
 #include "Dbms.h"
+#include <vector>
+#include <sstream>
 
 Dbms::Dbms(IDataBase* db) {
 	_db = db;
 }
 
 void Dbms::add(Employee data) {
-
+	_db->add(data);
 }
 
 list<Employee*> Dbms::del(Column type, string data) {
-	list<Employee*> list;
+	list<Employee*> list = _db->search(type, data);
+	_db->deleteTargets(list);
 	return list;
 }
 
 list<Employee*> Dbms::sch(Column type, string data) {
-	list<Employee*> list;
-	list = _db->search(type, data);
+	list<Employee*> list = _db->search(type, data);
 	return list;
 }
 
+vector<string> tokenize_getline(const string& data, const char delimiter = ' ') {
+	vector<string> result;
+	string token;
+	stringstream ss(data);
+
+	while (getline(ss, token, delimiter)) result.push_back(token);
+	
+	return result;
+}
+
+void editColumn(Employee* target, const Column type, const string data) {
+	vector<string> tokenizedStr;
+
+	switch (type) {
+	case Column::EMPLOYEE_NUM:
+		target->employeeNum = stoi(data);
+		break;
+	case Column::NAME:
+		tokenizedStr = tokenize_getline(data, ' ');
+		target->name.first = tokenizedStr.at(0);
+		target->name.first = tokenizedStr.at(1);
+		break;
+	case Column::CL:
+		if (data == "CL1")
+			target->cl = CL::CL1;
+		else if (data == "CL2")
+			target->cl = CL::CL2;
+		else if (data == "CL3")
+			target->cl = CL::CL3;
+		else if (data == "CL4")
+			target->cl = CL::CL4;
+		break;
+	case Column::PHONE:
+		tokenizedStr = tokenize_getline(data, '-');
+		target->phoneNum.start = stoi(tokenizedStr.at(0));
+		target->phoneNum.mid = stoi(tokenizedStr.at(1));
+		target->phoneNum.mid = stoi(tokenizedStr.at(2));
+		break;
+	case Column::BIRTHDAY:
+		target->birthDay.year = stoi(data.substr(0, 4));
+		target->birthDay.month = stoi(data.substr(4, 2));
+		target->birthDay.day = stoi(data.substr(6, 2));
+		break;
+	case Column::CERTI:
+		if (data == "ADV")
+			target->certi = CERTI::ADV;
+		else if (data == "PRO")
+			target->certi = CERTI::PRO;
+		else if (data == "EX")
+			target->certi = CERTI::EX;
+		break;
+	}
+}
+
 list<Employee*> Dbms::mod(Column typeFrom, string dataFrom, Column typeTo, string dataTo) {
-	list<Employee*> list;
+	list<Employee*> list = _db->search(typeFrom, dataFrom);
+
+	for (auto e : list) editColumn(e, typeTo, dataTo);
+
 	return list;
 }
