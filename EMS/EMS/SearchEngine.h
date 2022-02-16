f0ff16d27ec51c78b07e6c20c9c9576f45ce0945
jiminnn95 <jimin8338@gmail.com>
2022-02-16 12:52:10 +0900

    [Refactor] db 타입 변경 반영

--- EMS/EMS/SearchEngine.h
+++ EMS/EMS/SearchEngine.h
@@ -5,20 +5,20 @@
 class ISearchEngine {
 public:
 	virtual ~ISearchEngine() {}
-	virtual list<Employee*> search(list<Employee>& db, int employeeNum) const = 0;
-	virtual list<Employee*> search(list<Employee>& db, Name& name) const = 0;
-	virtual list<Employee*> search(list<Employee>& db, CL cl) const = 0;
-	virtual list<Employee*> search(list<Employee>& db, PhoneNum& phoneNum) const = 0;
-	virtual list<Employee*> search(list<Employee>& db, Birthday& birthDay) const = 0;
-	virtual list<Employee*> search(list<Employee>& db, CERTI certi) const = 0;
+	virtual list<Employee*> search(map<int, Employee>& db, int employeeNum) const = 0;
+	virtual list<Employee*> search(map<int, Employee>& db, Name& name) const = 0;
+	virtual list<Employee*> search(map<int, Employee>& db, CL cl) const = 0;
+	virtual list<Employee*> search(map<int, Employee>& db, PhoneNum& phoneNum) const = 0;
+	virtual list<Employee*> search(map<int, Employee>& db, Birthday& birthDay) const = 0;
+	virtual list<Employee*> search(map<int, Employee>& db, CERTI certi) const = 0;
 };
 
 class SearchEngine : public ISearchEngine {
 public:
-	virtual list<Employee*> search(list<Employee>& db, int employeeNum) const override;
-	virtual list<Employee*> search(list<Employee>& db, Name& name) const override;
-	virtual list<Employee*> search(list<Employee>& db, CL cl) const override;
-	virtual list<Employee*> search(list<Employee>& db, PhoneNum& phoneNum) const override;
-	virtual list<Employee*> search(list<Employee>& db, Birthday& birthDay) const override;
-	virtual list<Employee*> search(list<Employee>& db, CERTI certi) const override;
+	virtual list<Employee*> search(map<int, Employee>& db, int employeeNum) const override;
+	virtual list<Employee*> search(map<int, Employee>& db, Name& name) const override;
+	virtual list<Employee*> search(map<int, Employee>& db, CL cl) const override;
+	virtual list<Employee*> search(map<int, Employee>& db, PhoneNum& phoneNum) const override;
+	virtual list<Employee*> search(map<int, Employee>& db, Birthday& birthDay) const override;
+	virtual list<Employee*> search(map<int, Employee>& db, CERTI certi) const override;
 };
