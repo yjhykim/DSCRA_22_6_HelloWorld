c6351f1b4cc484690d8f979bf378ffda39d67041
kjh <cisc@kakao.com>
2022-02-17 15:21:59 +0900

    [Fix] mock Printer 변경
    printer인터페이스가 변경되어 mock 클래스 변경이 필요합니다.
    
    Signed-off-by: kjh <cisc@kakao.com>

--- EMS/TestEMS/mock_Printer.h
+++ EMS/TestEMS/mock_Printer.h
@@ -5,4 +5,5 @@
 class MockPrinter : public IPrinter {
 public:
     MOCK_METHOD(void, print, (std::list<Employee>& data, string& cmd), (const, override));
+    MOCK_METHOD(void, print, (int data, string& cmd), (const, override));
 };
