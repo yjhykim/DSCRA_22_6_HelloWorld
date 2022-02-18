a252140aa0b6d531035a34eff438d3818dfe3d61
안혜빈 <99627232+HyebinAN@users.noreply.github.com>
2022-02-18 13:46:10 +0900

    [Refactoring] 주석 제거

--- EMS/EMS/DataBase.cpp
+++ EMS/EMS/DataBase.cpp
@@ -69,50 +69,6 @@ bool DataBase::deleteTargets(list<Employee*> target) {
     return true;
 }
 
-//void editColumn(Employee* target, const Column type, const string data) {
-//	vector<string> tokenizedStr;
-//
-//	switch (type) {
-//	case Column::EMPLOYEE_NUM:
-//		target->employeeNum = stoi(data);
-//		break;
-//	case Column::NAME:
-//		tokenizedStr = split(data, ' ');
-//		target->name.first = tokenizedStr.at(0);
-//		target->name.last = tokenizedStr.at(1);
-//		break;
-//	case Column::CL:
-//		if (data == "CL1")
-//			target->cl = CL::CL1;
-//		else if (data == "CL2")
-//			target->cl = CL::CL2;
-//		else if (data == "CL3")
-//			target->cl = CL::CL3;
-//		else if (data == "CL4")
-//			target->cl = CL::CL4;
-//		break;
-//	case Column::PHONE:
-//		tokenizedStr = split(data, '-');
-//		target->phoneNum.start = stoi(tokenizedStr.at(0));
-//		target->phoneNum.mid = stoi(tokenizedStr.at(1));
-//		target->phoneNum.last = stoi(tokenizedStr.at(2));
-//		break;
-//	case Column::BIRTHDAY:
-//		target->birthDay.year = stoi(data.substr(INDEX_BIRTH_YEAR_START, LENGTH_BIRTH_YEAR));
-//		target->birthDay.month = stoi(data.substr(INDEX_BIRTH_MONTH_START, LENGTH_BIRTH_MONTH));
-//		target->birthDay.day = stoi(data.substr(INDEX_BIRTH_DAY_START, LENGTH_BIRTH_DAY));
-//		break;
-//	case Column::CERTI:
-//		if (data == "ADV")
-//			target->certi = CERTI::ADV;
-//		else if (data == "PRO")
-//			target->certi = CERTI::PRO;
-//		else if (data == "EX")
-//			target->certi = CERTI::EX;
-//		break;
-//	}
-//}
-
 bool DataBase::modifyTarget(EmployeeNum id, Column column, string data) {
     if (database_.count(id) == 0) {
         return false;
